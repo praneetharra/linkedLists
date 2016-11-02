4d4dbadd1ca0f9b4a3d38e480b95d67563b7b269
Praneeth Arra <parra@mtu.edu>
2016-11-02 14:51:34 -0400

    Create basic_LL.py

---
+++ basic_LL.py
@@ -0,0 +1,82 @@
+class Node(object):
+
+    def __init__ (self, d, n = None):
+        self.data = d
+        self.next_node = n
+
+    def get_next (self):
+        return self.next_node
+
+    def set_next (self, n):
+        self.next_node = n
+
+    def get_data (self):
+        return self.data
+
+    def set_data (self, d):
+        self.data = d
+
+
+class LinkedList (object):
+
+    def __init__(self, r = None):
+        self.root = r
+        self.size = 0
+
+    def get_size (self):
+        return self.size
+
+    def add (self, d):
+        new_node = Node (d, self.root)
+        self.root = new_node
+        self.size += 1
+
+    def remove (self, d):
+        this_node = self.root
+        prev_node = None
+
+        while this_node:
+            if this_node.get_data() == d:
+                if prev_node:
+                    prev_node.set_next(this_node.get_next())
+                else:
+                    self.root = this_node.get_next()
+                self.size -= 1
+                return True		# data removed
+            else:
+                prev_node = this_node
+                this_node = this_node.get_next()
+        return False  # data not found
+
+    def find (self, d):
+        this_node = self.root
+        while this_node:
+            if this_node.get_data() == d:
+                return d
+            else:
+                this_node = this_node.get_next()
+        return None
+        
+    def __str__(self):
+        a=[]
+        this_node = self.root
+        while this_node:
+            a.append(this_node.get_data())
+            this_node = this_node.get_next()
+        p = ''
+        for i in a[:len(a)-1]:
+            p = p + str(i) + '->'
+        p = p+str(a[len(a)-1])
+        return p
+            
+
+myList = LinkedList()
+myList.add(5)
+myList.add(8)
+myList.add(12)
+myList.add(15)
+myList.add(17)
+myList.add(16)
+myList.add(12)
+
+print(myList)
